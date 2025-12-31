da6ff170878ad77cc9d0976e0304bd307ed254f0
jojunhyeong <jojh0323@naver.com>
2025-12-31 17:28:41 +0900

    feat: custom breakpoint 설정 -> desktop, mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,19 @@ export default function Page(){
         <Link href="/products/1">
           <Button>제품 정보 페이지</Button>
         </Link>
+
+       <div
+      className="
+        h-40
+        bg-red-500
+        mobile:bg-blue-500
+        desktop:bg-green-500
+        flex items-center justify-center
+        text-white font-bold
+      "
+    >
+        Breakpoint Test
+    </div>
       </>
     )
 }
