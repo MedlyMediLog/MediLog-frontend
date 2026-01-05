52cef7d545849f57c3fbbeda1109896609defced
jojunhyeong <jojh0323@naver.com>
2026-01-05 11:36:19 +0900

    feat(layout): Footer 하단 배치 처리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,14 @@
-import type { ReactNode } from "react";
-import "./globals.css"
+import type { ReactNode } from 'react'
+import './globals.css'
+import Footer from './_components/Footer'
 
-export default function RootLayout({
-  children,
-}: {
-  children: ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko">
-      <body>
-        {children}
+      <body className="flex flex-col min-h-screen">
+        <main className='flex-1'> {children}</main>
       </body>
+      <Footer />
     </html>
-  );
+  )
 }
