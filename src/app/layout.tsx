5734b2772a0019f973b1f3a9b48c696a62929de2
jojunhyeong <jojh0323@naver.com>
2026-01-05 17:10:42 +0900

    feat: 사이드바 제작

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,25 @@
-import type { ReactNode } from 'react'
-import './globals.css'
-import Footer from './_components/Footer'
+import type { ReactNode } from "react"
+import "./globals.css"
+import Footer from "./_components/Footer"
+import SideBar from "./_components/SideBar"
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko">
-      <body className="flex flex-col min-h-screen">
-        <main className='flex-1'> {children}</main>
+      <body className="flex flex-col min-h-screen bg-[#edf2f6]">
+        <div className='flex flex-1'>
+            <aside className='hidden desktop:block shrink-0'>
+                <SideBar/>
+          </aside>
+
+          {/* Main */}
+          <main className="flex-1">
+            {children}
+          </main>
+        </div>
+
+        <Footer />
       </body>
-      <Footer />
     </html>
   )
 }
