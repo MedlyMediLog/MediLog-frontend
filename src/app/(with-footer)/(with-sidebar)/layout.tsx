d413b4c091e8977e559c0c3e5991821bfdfe96cb
shinhaeng1204@gmail.com <shinhaeng1204@gmail.com>
2026-01-21 10:40:16 +0900

    사이드바 폴더 구조 변경

--- src/app/(with-footer)/(with-sidebar)/layout.tsx
+++ src/app/(with-footer)/(with-sidebar)/layout.tsx
@@ -1,12 +1,8 @@
 import type { ReactNode } from 'react'
-import Footer from '../_components/Footer'
-import SideBar from '../_components/common/SideBar/SideBar'
+import Footer from '../../_components/Footer'
+import SideBar from '../../_components/common/SideBar/SideBar'
 
-export default function WithFooterLayout({
-  children,
-}: {
-  children: ReactNode
-}) {
+export default function WithFooterLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
       <div className="flex flex-1">
